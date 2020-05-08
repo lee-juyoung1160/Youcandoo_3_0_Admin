64fb375be0e88483d33366bda052a7a73b3d95af
jeensanity <iamjeen@yanadoo.co.kr>
2020-05-08 17:38:18 +0900

    프로모션 등록 기업명 autocomplete

--- assets/js/const.js
+++ assets/js/const.js
@@ -22,7 +22,8 @@
 	const headers = { "Authorization" : "9c3a60d74726c4e1cc0732fd280c89dbf80a344e7c3dc2c4ad4fdf12b97e52c7" };
 	const baseApiUrl = 'http://api.kakaokids.org/v1.0/admin/';
 	const api = {
-		createAdmin : baseApiUrl+''
+		listBizName : baseApiUrl+'keyword/getCompanyName'
+		,createAdmin : baseApiUrl+''
 		,deleteAdmin : ''
 		,updateAdmin : ''
 		,listAdmin : ''

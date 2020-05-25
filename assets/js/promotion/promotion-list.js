e66bd3ff72af1617d0a2a30fb551c6325a575457
jeensanity <iamjeen@yanadoo.co.kr>
2020-05-25 18:52:32 +0900

    프로모션 목록

--- assets/js/promotion/promotion-list.js
+++ assets/js/promotion/promotion-list.js
@@ -57,12 +57,20 @@
 				}
 			},
 			columns: [
-				{title: "No", 	data: "idx",    width: "5%",    orderable: false,   className: "text-center" }
-				/*,{title: "등록일", 	data: "created",     name: "created",     orderable: false,   className: "text-center",
+				{title: "기업", 			data: "nickname",    		   width: "15%",    orderable: false,   className: "text-center" }
+				,{title: "프로모션명", 	data: "promotion_title",       width: "30%",    orderable: false,   className: "text-center" }
+				,{title: "프로모션기간", 	data: "start_date",    		   width: "20%",    orderable: false,   className: "text-center" }
+				,{title: "프로모션예산", 	data: "budget_ucd",    		   width: "15%",    orderable: false,   className: "text-center",
 					render: function (data) {
-						return data.substring(0, 10);
+						return numberWithCommas(data);
 					}
-				}*/
+				}
+				,{title: "프로모션잔여예산", 	data: "remain_budget_ucd", width: "15%",    orderable: false,   className: "text-center",
+					render: function (data) {
+						return numberWithCommas(data);
+					}
+				}
+				,{title: "배너 여부", 	data: "is_banner",    width: "10%",    orderable: false,   className: "text-center"}
 			],
 			language: {
 				emptyTable : message.emptyList
@@ -95,7 +103,6 @@
 			},
 			fnRowCallback: function( nRow, aData ) {
 				setRowAttributes(nRow, aData);
-				console.log(aData);
 			}
 		});
 	}
@@ -112,16 +119,28 @@
 			,"keyword" : keyword.val()
 		}
 
-		return {"data": JSON.stringify(param)};
+		return JSON.stringify(param);
 	}
 
 	function setRowAttributes(nRow, aData)
 	{
-		let titleDom = $(nRow).children().eq(0);
+		let titleDom  = $(nRow).children().eq(1);
+		let periodDom = $(nRow).children().eq(2);
+		let btnDom 	  = $(nRow).children().eq(5);
 		let detailUrl = '/pro/detail/'+aData.idx;
 
-		// 제목에 a 태그 추가
-		//titleDom.html('<a href="'+detailUrl+'">'+aData.title+'</a>');
+		/** 제목에 a 태그 추가 **/
+		titleDom.html('<a href="'+detailUrl+'">'+aData.promotion_title+'</a>');
+
+		/** 프로모션 기간 **/
+		periodDom.html(aData.start_date +' ~ '+aData.end_date);
+
+		/** 배너보기 버튼 **/
+		let bannerUrl = aData.banner_image_url;
+		let listUrl   = aData.list_image_url;
+		let introUrl  = aData.intro_image_url;
+		let innerDom = '<button onclick="viewImage(this);" type="button" class="more-infor-btn" data-banner="'+bannerUrl+'" data-list="'+listUrl+'" data-intro="'+introUrl+'">보기</button>';
+		btnDom.html(innerDom);
 	}
 
 	function onSubmitSearch()
@@ -129,10 +148,47 @@
 		buildGrid();
 	}
 
+	function viewImage(obj)
+	{
+		let bannerImage = $(obj).data('banner');
+		let listImage   = $(obj).data('list');
+		let introImage  = $(obj).data('intro');
+
+		let modal = '';
+		modal += '<div id="viewImageModal" class="modal-content modal-01" style="display: block;">';
+		modal += 	'<div class="wrap">';
+		modal += 		'<i onclick="removeModal();" class="close-btn fas fa-times-circle"></i>';
+		modal += 		'<p class="modal-title">프로모션 이미지</p>';
+		modal += 		'<ul class="modal-information">';
+		modal += 			'<li>';
+		modal += 				'<p class="sub-title">배너 및 리스트</p>';
+		modal += 				'<p class="data-contents">';
+		modal += 					'<img src="'+bannerImage+'" alt="배너 및 소개 이미지">';
+		modal +=	 			'</p>';
+		modal += 			'</li>';
+		modal += 			'<li>';
+		modal += 				'<p class="sub-title">소개이미지</p>';
+		modal += 				'<p class="data-contents">';
+		modal += 					'<img src="'+introImage+'" alt="소개이미지">';
+		modal += 				'</p>';
+		modal += 			'</li>';
+		modal += 		'</ul>';
+		modal += 	'</div>';
+		modal += '</div>';
+		modal += '<div onclick="removeModal();" id="modalBackDrop" class="modal-layout" style="display: block;"></div>';
+
+		$('body').append(modal);
+	}
+
+	function removeModal()
+	{
+		$("#viewImageModal").remove();
+		$("#modalBackDrop").remove();
+	}
+
 	function onClickExcelBtn()
 	{
 		getExcelData();
-
 	}
 
 	function getExcelData()
@@ -147,24 +203,22 @@
 			error: function (request, status) {
 				console.log(status);
 			}
-
 		});
 	}
 
 	function excelParams()
 	{
 		let param = {
-			"limit" : 10000
+			"limit" : 20000
 			,"page" : 1
-			,"date_type" : dateType.val()
-			,"from_date" : dateFrom.val()
-			,"to_date" : dateTo.val()
-			,"search_type" : searchType.val()
+			,"dateType" : dateType.val()
+			,"fromDate" : dateFrom.val()
+			,"toDate" : dateTo.val()
+			,"searchType" : searchType.val()
 			,"keyword" : keyword.val()
-			//,type_opt : $('#selType').val()
 		}
 
-		return {"data": JSON.stringify(param)};
+		return JSON.stringify(param);
 	}
 
 

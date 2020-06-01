2230afb89bbeaf74d004096ac5f6ab0faaab580f
jeensanity <iamjeen@yanadoo.co.kr>
2020-06-01 10:20:27 +0900

    비즈 상세: 프로모션명 클릭 > 프로모션 상세 이동, 프로모션 상세 리워드조건 참여자수 표출 오류 수정

--- assets/js/biz/biz-detail.js
+++ assets/js/biz/biz-detail.js
@@ -132,8 +132,12 @@
 
 		function setRowAttributes(nRow, aData)
 		{
+			let titleDom = $(nRow).children().eq(1);
 			let periodDom = $(nRow).children().eq(3);
+			let detailUrl = page.detailPromo+aData.idx;
 
+			/** 제목에 a 태그 추가 **/
+			$(titleDom).html('<a href="'+detailUrl+'">'+aData.promotion_title+'</a>');
 			/** 프로모션 기간 **/
 			$(periodDom).html(aData.start_date+' ~ '+aData.end_date);
 		}

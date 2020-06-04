e0b7a4f7e8ac5631d5595faee9864f4edc4b1ae7
jeensanity <iamjeen@yanadoo.co.kr>
2020-06-04 11:05:08 +0900

    마이페이지 ui 변경관련 스크립트 수정

--- assets/js/admin/profile.js
+++ assets/js/admin/profile.js
@@ -1,6 +1,4 @@
 
-	const btnProfileModify  = $("#btnProfileModify");
-	const myProfile			= $("#myProfile");
 	const myId 				= $("#myId");
 	const myName 			= $("#myName");
 	const myEmail 			= $("#myEmail");
@@ -8,44 +6,15 @@
 	const passwordChk 		= $("#passwordChk");
 	const passwordChkTxt	= $("#passwordChkTxt");
 	const btnSubmit 		= $("#btnSubmit");
-	const search 			= $(".search");
-	const reset 			= $(".reset");
-	const selPageLength		= $("#selPageLength");
-	const dataNum			= $(".data-num");
-	const dataTable			= $("#dataTable");
 
 	$(document).ready(function () {
-		/** 데이트피커 초기화 **/
-		initSearchDatepicker();
-		/** 검색 폼 초기화 **/
-		initSearchForm();
 		/** 나의 정보 **/
 		getProfile();
-		/** 나의 이력 **/
-		getLog();
 		/** 이벤트 **/
-		btnProfileModify	.on('click', function () { toggleProfile(this); })
-		btnSubmit			.on('click', function () { onSubmitProfile(this); })
-		search				.on("click", function () { getLog(); });
-		reset				.on("click", function () { initSearchForm(); });
-		selPageLength		.on("change", function () { getLog(); });
-		dayButtons      	.on("click", function () { onClickActiveAloneDayBtn(this); });
-		password      		.on("keyup", function () { onKeyupPassword(); });
-		passwordChk      	.on("keyup", function () { onKeyupPasswordChk(); });
+		password    .on("keyup", function () { onKeyupPassword(); });
+		passwordChk .on("keyup", function () { onKeyupPasswordChk(); });
+		btnSubmit	.on('click', function () { onSubmitProfile(this); })
 	});
-
-	function initSearchForm()
-	{
-		/** 검색범위 초기화 **/
-		onClickActiveAloneDayBtn($(".btn_week"));
-	}
-
-	/** 프로필 상세 영역 toggle **/
-	function toggleProfile(obj)
-	{
-		$(obj).toggleClass('active');
-		myProfile.toggleClass('active');
-	}
 
 	function onKeyupPassword()
 	{
@@ -149,71 +118,4 @@
 				});
 			}
 		}
-	}
-
-	function getLog()
-	{
-		dataTable.DataTable({
-			ajax : {
-				url: api.listMyLog,
-				type: "POST",
-				headers: headers,
-				data: function (d) {
-					return tableParams(d);
-				},
-				error: function (request, status) {
-					alert(label.list+message.ajaxLoadError);
-				}
-			},
-			columns: [
-				{title: "경로", 		data: "url",   	 			width: "40%",      	orderable: false,   className: "text-center cursor-default" }
-				,{title: "날짜", 	data: "datetime",   		width: "15%", 		orderable: false,   className: "text-center cursor-default" }
-				,{title: "구분", 	data: "access_type_name",	width: "15%",    	orderable: false,   className: "text-center cursor-default" }
-				,{title: "활동", 	data: "action",      		width: "15%",    	orderable: false,   className: "text-center cursor-default" }
-			],
-			language: {
-				emptyTable : message.emptyList
-				,zeroRecords: message.emptyList
-				,processing : message.searching
-				,paginate: {
-					previous: label.previous
-					,next: label.next
-				}
-			},
-			processing: false,
-			serverSide: true,
-			paging: true,
-			pageLength: Number(selPageLength.val()),
-			/*pagingType: "simple_numbers_no_ellipses",*/
-			ordering: false,
-			order: [],
-			info: false,
-			select: false,
-			lengthChange: false,
-			autoWidth: false,
-			searching: false,
-			fixedHeader:false,
-			destroy: true,
-			initComplete: function () {
-				let table = dataTable.DataTable();
-				let info = table.page.info();
-
-				dataNum.text(info.recordsTotal);
-			},
-			fnRowCallback: function( nRow, aData ) {
-			}
-		});
-	}
-
-	function tableParams(d)
-	{
-		let param = {
-			"limit" : d.length
-			,"page" : (d.start / d.length) + 1
-			,"from_date" : dateFrom.val()
-			,"to_date" : dateTo.val()
-			,"userid" : sessionUserId.val()
-		}
-
-		return JSON.stringify(param);
 	}

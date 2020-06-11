6c169d9be527481edeb2a7c55214c134ad55d334
gang-gayeong <kathy@kakaokids.com>
2020-06-11 15:18:33 +0900

    ㅇㅇ

--- assets/js/review/review-list.js
+++ assets/js/review/review-list.js
@@ -21,6 +21,8 @@ document.addEventListener("DOMContentLoaded", function () {
     ratingLists.forEach(function(item, index, array){item.checked = true;});
     document.querySelector('input[name=radio-report]').checked = true;
     document.querySelector('input[name=radio-blind]').checked = true;
+    // 테이블 실행
+    getReviewListData();
 });
 /** 검색 필드 reset **/
 resetBtn.addEventListener('click', () => {
@@ -67,6 +69,10 @@ function getReviewListData(params) {
         ordering: false, //원하는 순서대로 데이터 표시
         paging: true, //페이징
         destroy: true, //기존 테이블을 삭제하고 새 옵션으로 바꿈
+        select: {
+            style: 'multi',
+            selector: ':checkbox'
+        },
         pageLength: Number(limits.value),
         serverSide: true, // true = 서버쪽으로 페이지네이션 처리 요청(페이지 이동시 서버호출함), false = 전체 데이터를 불러워서 datatable 을 이용하여 웹에서 페이지네이션 처리(페이지 이동시 서버호출하지 않음)
         initComplete: function () {
@@ -90,8 +96,12 @@ function getReviewListData(params) {
                 console.log(c)
             },
         },columns: [
+            {data: "idx", title: tableCheckAllDom(), render: function (data) {
+                    return multiCheckBoxDom(data);
+                    console.log(data)
+                }
+            },
             {data: "review_text", render : function(data, type, full, meta) {
-                    // 뒤에 '' 사이에추가하고싶은거 추가해서 넣으면됨.
                     return "<a href=\"javascript: openModal('"+data+"', '"+full.rating+"', '"+full.doit_title+"', '"+full.report_count+"', '"+full.is_blind+"', '"+full.created+"', '"+full.nickname+"')\">"+data+"</a>";
                 }
             },
@@ -124,12 +134,21 @@ function openModal (review_text, rating, doit_title, report_count, is_blind, cre
     $(".modal-layout").fadeIn(500);
     $("#modalDetail").fadeIn(500);
 
+    // 모달 평점에 따른 별 추가
+    let liList = $('ol.star-wrap').find('li');
+    for(let i=0; i<liList.length; i++){
+        let listObj = $(liList[i]);
+        if(i<rating){
+            listObj.addClass('on');
+        } else {
+            listObj.removeClass('on');
+        }
+    }
 }
 function closeModal(){
      $(".modal-layout").fadeOut(500);
      $("#modalDetail").fadeOut(500);
  }
-
 const modalCloseBtn = document.querySelector('.modal-content .close-btn');
 const modalLayout = document.querySelector('.modal-layout');
 modalCloseBtn.addEventListener('click', () => {closeModal();});

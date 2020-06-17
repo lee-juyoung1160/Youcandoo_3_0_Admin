fda4bd1ee2c6914a7287ed164801aa30b9fff399
jeensanity <iamjeen@yanadoo.co.kr>
2020-06-17 19:06:21 +0900

    ucd 출금내역 목록

--- assets/js/const.js
+++ assets/js/const.js
@@ -190,6 +190,7 @@
 		,getMenuByAuth : baseApiUrl+'auth/get/menu'
 		/** UCD **/
 		,listUseUcd : baseApiUrl+'ucd/use/list'
+		,listWithdrawUcd : baseApiUrl+'ucd/withdraw/list'
 		,createBizUcd : baseApiUrl+'ucd/create/company'
 		,createUserUcd : baseApiUrl+'ucd/create/user'
 	}

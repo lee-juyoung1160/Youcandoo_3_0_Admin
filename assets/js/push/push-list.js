8c535da4757077c284e391ee30b968caacad8718
jeensanity <iamjeen@yanadoo.co.kr>
2020-06-22 17:42:39 +0900

    ..

--- assets/js/push/push-list.js
+++ assets/js/push/push-list.js
@@ -113,14 +113,14 @@
 		return JSON.stringify(param);
 	}
 
-	function setRowAttributes(nRow, aData)
+	/*function setRowAttributes(nRow, aData)
 	{
 		let titleDom = $(nRow).children().eq(2);
 		let detailUrl = '/review/detail/'+aData.idx;
 
-		/** 제목에 a 태그 추가 **/
+		/!** 제목에 a 태그 추가 **!/
 		$(titleDom).html('<a href="'+detailUrl+'">'+aData.title+'</a>');
-	}
+	}*/
 
 	function onSubmitSearch()
 	{

64922fbd774589b4ad5697b41388807c745cbbf5
jeensanity <iamjeen@yanadoo.co.kr>
2020-06-23 17:44:50 +0900

    ..

--- assets/js/doit/doit-detail.js
+++ assets/js/doit/doit-detail.js
@@ -470,7 +470,7 @@
 				,{title: "레드카드",    		data: "red",   			width: "10%",    orderable: false,   className: "text-center cursor-default" }
 				,{title: "평균달성률(%)", 	data: "avg_percent",    width: "10%",    orderable: false,   className: "text-center cursor-default",
 					render: function (data) {
-						return Number(data) < 100 && Number(data) > 0 ? Number(data).toFixed(1) : data;
+						return Math.floor(Number(data));
 					}
 				}
 				,{title: "적립리워드(UCD)",  	data: "total_reward",   width: "10%",    orderable: false,   className: "text-center cursor-default" }

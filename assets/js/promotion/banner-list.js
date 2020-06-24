5c4d8bc749fcbf399199b807c6e72ea081f7f83d
ljy88 <noocgs@gmail.com>
2020-06-24 11:06:05 +0900

    프로모션 배너

--- assets/js/promotion/banner-list.js
+++ assets/js/promotion/banner-list.js
@@ -8,7 +8,7 @@
 
 	$(document).ready(function () {
 		/** 테이블 데이터 로드 **/
-		buildGrid();
+		//buildGrid();
 		/** 이벤트 **/
 		modalCloseBtn	.on('click', function () { modalFadeout(); });
 		modalLayout		.on('click', function () { modalFadeout(); });
@@ -22,7 +22,7 @@
 
 	function initModal()
 	{
-		getPromo();
+		//getPromo();
 	}
 
 	function buildGrid()

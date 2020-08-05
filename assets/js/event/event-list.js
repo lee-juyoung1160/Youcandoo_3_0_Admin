0f32dcb9e5a4d421951b34128b6746fb7b889110
jeensanity <iamjeen@yanadoo.co.kr>
2020-08-05 16:05:38 +0900

    ..

--- assets/js/event/event-list.js
+++ assets/js/event/event-list.js
@@ -73,7 +73,7 @@
 				}
 			},
 			columns: [
-				{title: "", 		data: "idx",   			   width: "5%",     className: "no-sort",
+				{title: "", 		data: "idx",   			   width: "5%",     className: "cursor-default no-sort",
 					render: function (data) {
 						return singleCheckBoxDom(data);
 					}

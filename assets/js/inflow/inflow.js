10aeebd266aaf920a7e3c318ede900d1932fd42d
jeen <iamjeen@yanadoocorp.co.kr>
2020-09-07 17:09:16 +0900

    ..

---
+++ assets/js/inflow/inflow.js
@@ -0,0 +1,125 @@
+
+	const search 		= $(".search");
+	const reset 		= $(".reset");
+	const dataTable		= $("#dataTable")
+	const searchType 	= $("#search_type");
+	const keyword		= $("#keyword");
+	const selPageLength = $("#selPageLength");
+
+	$( () => {
+		/** 데이트피커 초기화 **/
+		initSearchDatepicker();
+		/** 상단 검색 폼 초기화 **/
+		initSearchForm();
+		/** n개씩 보기 초기화 (initSearchForm 이후에 와야 함) **/
+		initPageLength(selPageLength);
+		/** 목록 불러오기 **/
+		/*buildGrid();*/
+		/** 이벤트 **/
+		$("body")  .on("keydown", function (event) { onKeydownSearch(event) });
+		search			.on("click", function () { onSubmitSearch(); });
+		reset			.on("click", function () { initSearchForm(); });
+		selPageLength	.on("change", function () { onSubmitSearch(); });
+		dayButtons      .on("click", function () { onClickActiveAloneDayBtn(this); });
+	});
+
+	function initSearchForm()
+	{
+		keyword.val('');
+		initSelectOption();
+		initSearchDateRange();
+		initMaxDateToday();
+		initDayBtn();
+	}
+
+	function buildGrid()
+	{
+		dataTable.DataTable({
+			ajax : {
+				url: api.listEvent,
+				type: "POST",
+				headers: headers,
+				data: function (d) {
+					return tableParams();
+				},
+				error: function (request, status) {
+					sweetError(label.list+message.ajaxLoadError);
+				}
+			},
+			columns: [
+				{title: "", 		data: "",    	   		width: "25%",	className: "cursor-default no-sort",
+					render: function (data, type, row, meat) {
+						console.log(row)
+						return "";
+					}
+				}
+				,{title: "AOS", 	data: "event_type",    	width: "25%",	className: "cursor-default" }
+				,{title: "IOS", 	data: "event_type",    	width: "25%",	className: "cursor-default" }
+				,{title: "WEB", 	data: "event_type",    	width: "25%",	className: "cursor-default" }
+			],
+			language: {
+				emptyTable : message.emptyList
+				,zeroRecords: message.emptyList
+				,processing: message.searching
+				,paginate: {
+					previous: label.previous
+					,next: label.next
+				}
+			},
+			processing: false,
+			serverSide: true,
+			paging: true,
+			pageLength: Number(selPageLength.val()),
+			/*pagingType: "simple_numbers_no_ellipses",*/
+			ordering: false,
+			order: [],
+			info: false,
+			select: {
+				style: 'single',
+				selector: ':checkbox'
+			},
+			lengthChange: false,
+			autoWidth: false,
+			searching: false,
+			fixedHeader: false,
+			destroy: false,
+			initComplete: function () {
+				initTableSorter(this);
+			},
+			fnRowCallback: function( nRow, aData ) {
+			},
+			drawCallback: function (settings) {
+				buildTotalCount(this);
+				toggleBtnPreviousAndNextOnTable(this);
+			}
+		});
+	}
+
+	function tableParams()
+	{
+		let table = dataTable.DataTable();
+		let info = table.page.info();
+		let _page = (info.start / info.length) + 1;
+
+		let param = {
+			"limit" : Number(selPageLength.val())
+			,"page" : _page
+			,"fromDate" : dateFrom.val()
+			,"toDate" : dateTo.val()
+			,"searchType" : searchType.val()
+			,"keyword" : keyword.val()
+		}
+
+		/** sessionStorage에 정보 저장 : 뒤로가기 액션 히스토리 체크용 **/
+		setHistoryParam(param);
+
+		return JSON.stringify(param);
+	}
+
+	function onSubmitSearch()
+	{
+		let table = dataTable.DataTable();
+		table.page.len(Number(selPageLength.val()));
+		table.ajax.reload();
+		initMaxDateToday();
+	}

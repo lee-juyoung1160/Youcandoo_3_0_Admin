2dfe5da43eca82b00205c6edcf019531e114ee02
jeen <iamjeen@yanadoocorp.com>
2020-10-07 12:06:11 +0900

    ..

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -7,14 +7,11 @@ class Auth extends CI_Controller {
     {
         parent::__construct();
         $this->load->library('session');
-        $this->redis_session = new Redis();
-        $this->redis_session->connect($this->config->item('redis_session'));
     }
     public function __destruct()
     {
         // TODO: Implement __destruct() method.
-        $this->redis_session->close();
-    } 
+    }
 
     /**
      * 1. 로그인
@@ -23,34 +20,17 @@ class Auth extends CI_Controller {
     {
         $UserID = $this->input->post("userid");
         $Password = $this->input->post("password");
-        if($UserID == "")
-        {
+        if($UserID == "") {
             alert("ID 값이 입력되지 않았습니다", "/main/login");
             return;
         }
-        if($Password == "")
-        {
+        if($Password == "") {
             alert("Password 값이 입력되지 않았습니다", "/main/login");
             return;
         }
 
         $Password = hash("sha512",$Password);
-        if($this->redis_session->exists("admin:user"))
-        {
-            $IsExists = $this->redis_session->hExists("admin:user", $UserID);
-            if(!$IsExists)
-            {
-                alert("사용자 정보가 존재하지 않습니다", "/main/login");
-                return;
-            }
-
-            $UserData = json_decode($this->redis_session->hGet("admin:user", $UserID));
-        }
-        else
-        {
-            $Response = $this->getAdminUserData($UserID);
-            $UserData = $Response->data;
-        }
+        $UserData = $this->getAdminUserData($UserID);
 
         if ($Password != $UserData->password) {
             $this->updateFailCount($UserID, 1);
@@ -63,8 +43,35 @@ class Auth extends CI_Controller {
             return;
         }
 
-        $this->setLoginInfo($UserID);
-        $this->updateFailCount($UserID, 0);
+        if($UserData->mfa_yn=="Y"){
+            if($UserData->status=="승인대기"){
+                alert("승인되지 않은 사용자입니다", "/main/login");
+                return;
+            }
+            require_once APPPATH.'third_party/PHPGangsta/GoogleAuthenticator.php';
+            $ga = new PHPGangsta_GoogleAuthenticator();
+            $secret = empty($UserData->mfa_key)? $ga->createSecret():$UserData->mfa_key;
+
+            $this->load->view('/login/mfa',array(
+                "secret"=>$secret,
+                "qrcode_url"=>$ga->getQRCodeGoogleUrl('YOUCANDOO', $secret),
+                "type"=>"login",
+                "userid"=>$UserID
+            ));
+            return;
+        }
+        $this->login_after($UserData);
+    }
+
+    private function login_after($UserData, $Secret=""){
+        $UserID = $UserData->userid;
+
+        if(!$this->setLoginInfo($UserID, $Secret)){
+            return;
+        }
+        if(!$this->updateFailCount($UserID, 0)){
+            return;
+        }
 
         // Cookie Set
         $this->load->helper('cookie');
@@ -102,7 +109,116 @@ class Auth extends CI_Controller {
             redirect('/', 'refresh');
         }
 
+    }
 
+    public function join()
+    {
+        $UserID = $this->input->post("userid");
+        $Password = $this->input->post("password");
+        $UserName = $this->input->post("username");
+        $UserEmail = $this->input->post("useremail");
+        $Url = "/main/join";
+
+        if($UserID == "") {
+            alert("ID 값이 입력되지 않았습니다", $Url);
+            return;
+        }
+        if($Password == "") {
+            alert("Password 값이 입력되지 않았습니다", $Url);
+            return;
+        }
+        if($UserName == "") {
+            alert("사용자 이름이 입력되지 않았습니다", $Url);
+            return;
+        }
+        if($UserEmail == "") {
+            alert("이메일 값이 입력되지 않았습니다", $Url);
+            return;
+        }
+
+        // 중복여부
+        if(!($this->curl(array("userid" => $UserID), "/v1.0/admin/getExistsAdminUser"))){
+            return;
+        }
+
+        // qrcode
+        require_once APPPATH.'third_party/PHPGangsta/GoogleAuthenticator.php';
+        $ga = new PHPGangsta_GoogleAuthenticator();
+        $secret = $ga->createSecret();
+
+        $this->load->view('/login/mfa', array(
+            "userid"=>$UserID,
+            "password"=>hash("sha512",$Password),
+            "username"=>$UserName,
+            "useremail"=>$UserEmail,
+            "qrcode_url"=>$ga->getQRCodeGoogleUrl('YOUCANDOO', $secret),
+            "secret"=>$secret,
+            "type"=>"join"
+        ));
+
+    }
+
+    public function mfa(){
+        $Secret = $this->input->post('secret');
+        $OTP = $this->input->post("otp");
+        $Type = $this->input->post("type");
+        $UserID = $this->input->post("userid");
+        $checkResult = $this->verify($Secret, $OTP);
+
+        if ($checkResult) {
+            if($Type=="login"){
+                $UserData = $this->getAdminUserData($UserID);
+                $this->login_after($UserData,$Secret);
+            }else{ // join
+                $Body = array(
+                    "userid" => $UserID,
+                    "password"=>$this->input->post("password"),
+                    "username"=>$this->input->post("username"),
+                    "useremail"=>$this->input->post("useremail"),
+                    "secret"=>$Secret
+                );
+                if($this->curl($Body, "/v1.0/admin/admin/create")){
+                    alert("회원가입 되었습니다.","/main/login");
+                }
+            }
+
+        } else {
+            alert("인증번호를 확인해주세요");
+        }
+    }
+
+    private function verify($Secret, $OTP){
+        $tolerance = 0;
+        //Every otp is valid for 30 sec.
+        // If somebody provides OTP at 29th sec, by the time it reaches the server OTP is expired.
+        //So we can give tolerance =1, it will check current  & previous OTP.
+        // tolerance =2, verifies current and last two OTPS
+
+        require_once APPPATH.'third_party/PHPGangsta/GoogleAuthenticator.php';
+        $ga = new PHPGangsta_GoogleAuthenticator();
+
+        return $ga->verifyCode($Secret, $OTP, $tolerance);    // 2 = 2*30sec clock tolerance
+    }
+
+    private function curl($Body, $Url){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->config->item("api_server_url").$Url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER,
+            array("Content-Type : application/json", "Authorization : 9c3a60d74726c4e1cc0732fd280c89dbf80a344e7c3dc2c4ad4fdf12b97e52c7"));
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($Body));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        $ResponseObj = json_decode($response);
+
+        $Success = ($ResponseObj->status == 30000);
+        if($ResponseObj->status != 30000) {
+            alert($ResponseObj->msg);
+        }
+        return (isset($ResponseObj->data))? $ResponseObj->data: $Success;
     }
 
     /**
@@ -121,7 +237,7 @@ class Auth extends CI_Controller {
     /**
      * 3. 접속 기록 업데이트
      */
-    public function setLoginInfo($UserID)
+    public function setLoginInfo($UserID, $Secret="")
     {
         $LoginIP="";
         if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
@@ -132,31 +248,9 @@ class Auth extends CI_Controller {
             $LoginIP = $_SERVER['REMOTE_ADDR'];
         }
 
-        $body = array(
-            "userid" => $UserID,
-            "ip" => $LoginIP
-        );
-        $header = array(
-            "Content-Type : application/json",
-            "Authorization : 9c3a60d74726c4e1cc0732fd280c89dbf80a344e7c3dc2c4ad4fdf12b97e52c7"
-        );
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->config->item("api_server_url")."/v1.0/admin/setLoginInfo");
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($ch);
-        curl_close($ch);
-        $ResponseObj = json_decode($response);
-        if($ResponseObj->status != 30000)
-        {
-//            echo json_encode($ResponseObj);
-            alert("접속 기록이 정상적으로 처리되지 않았습니다", "/");
-            return;
-        }
+        $Body = array("userid" => $UserID, "ip" => $LoginIP, "secret"=>$Secret);
+
+        return $this->curl($Body, "/v1.0/admin/setLoginInfo");
     }
 
     /**
@@ -164,31 +258,7 @@ class Auth extends CI_Controller {
      */
     public function updateFailCount($UserID, $Count)
     {
-        $body = array(
-            "userid" => $UserID,
-            "count" => $Count
-        );
-        $header = array(
-            "Content-Type : application/json",
-            "Authorization : 9c3a60d74726c4e1cc0732fd280c89dbf80a344e7c3dc2c4ad4fdf12b97e52c7"
-        );
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->config->item("api_server_url")."/v1.0/admin/setFailCount");
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($ch);
-        curl_close($ch);
-        $ResponseObj = json_decode($response);
-        if($ResponseObj->status != 30000)
-        {
-//            echo json_encode($ResponseObj);
-            alert("접속 기록이 정상적으로 처리되지 않았습니다", "/");
-            return;
-        }
+        return $this->curl(array("userid" => $UserID, "count" => $Count), "/v1.0/admin/setFailCount");
     }
 
     /**
@@ -196,29 +266,6 @@ class Auth extends CI_Controller {
      */
     public function getAdminUserData($UserID)
     {
-        $body = array(
-            "userid" => $UserID
-        );
-        $header = array(
-            "Content-Type : application/json",
-            "Authorization : 9c3a60d74726c4e1cc0732fd280c89dbf80a344e7c3dc2c4ad4fdf12b97e52c7"
-        );
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->config->item("api_server_url")."/v1.0/admin/getAdminUserData");
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($ch);
-        curl_close($ch);
-        $ResponseObj = json_decode($response);
-        if($ResponseObj->status != 30000)
-        {
-//            echo json_encode($ResponseObj);
-            alert($ResponseObj->msg, "/");
-            return;
-        }
+        return $this->curl(array("userid" => $UserID), "/v1.0/admin/getAdminUserData");
     }
 }

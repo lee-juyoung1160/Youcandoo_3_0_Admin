c4c3d08088db6ff0e1934a65ee6c01a7b43dca7c
leo <leo@yanadoocorp.com>
2020-10-28 13:48:48 +0900

    [Test] Git연동 테스트

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -16,6 +16,6 @@ class User extends MY_Controller {
     public function detail()
     {
         $data['profile_uuid'] = $this->input->post('uuid');
-        $this->load->layout('user/user-detail', $data);
+        $this->load->layout('user/user-detail', $data); 
     }
 }

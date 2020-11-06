785868e6b0df5a333d450dbb40e4ca0ec5853949
leo <leo@yanadoocorp.com>
2020-11-06 14:39:12 +0900

    [Add] doit/talk, doit/talkDetail URL 추가

---
+++ application/controllers/Gift.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Gift extends MY_Controller
+{
+
+    public function index()
+    {
+        $this->load->layout('gift/gift-list');
+    }
+
+    public function create()
+    {
+        $this->load->layout('gift/gift-create');
+    }
+
+    public function update()
+    {
+        $this->load->layout('gift/gift-update');
+    }
+
+    public function detail()
+    {
+        $this->load->layout('gift/gift-detail');
+    }
+
+    public function approval()
+    {
+        $this->load->layout('gift/gift-apply-list');
+    }
+
+}

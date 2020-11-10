2a727a754be0c26e61dd465e6e4cac65f6aa0967
leo <leo@yanadoocorp.com>
2020-11-10 18:23:47 +0900

    [Fix] autoload 수정

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -6,7 +6,7 @@ class Auth extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
-        $this->load->library('session');
+//        $this->load->library('session');
     }
     public function __destruct()
     {

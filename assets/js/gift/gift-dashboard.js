9a1fab7d87a7afb9f58584e047d06e4ac3f40892
jeen <iamjeen@yanadoocorp.com>
2020-12-24 11:16:39 +0900

    상품현황 진행중

--- assets/js/gift/gift-dashboard.js
+++ assets/js/gift/gift-dashboard.js
@@ -8,11 +8,15 @@
 
     /** 로드 바로 실행 **/
     $(() => {
+        /** 셀렉트박스 초기화 **/
         initSelectBox();
+        /** dataTable default config **/
+        initTableDefault();
+        /** 목록 불러오기 **/
         buildGrid();
-        /** 월단위 셀렉박스 이벤트 **/
-        /*selYear     .on('change', function () { updatePage(); });
-        selMonth    .on('change', function () { updatePage(); });*/
+        /** 이벤트 **/
+        selYear     .on('change', function () { onChangeSelectBox(); });
+        selMonth    .on('change', function () { onChangeSelectBox(); });
     });
 
     function initSelectBox()
@@ -47,3 +51,56 @@
 
         onChangeSelectOption(selMonth);
     }
+
+    function onChangeSelectBox()
+    {
+        let table = dataTable.DataTable();
+        table.ajax.reload();
+    }
+
+    function buildGrid()
+    {
+        dataTable.DataTable({
+            ajax : {
+                url: api.dashboardGift,
+                type: "POST",
+                headers: headers,
+                data: function (d) {
+                    return tableParams();
+                },
+                error: function (request, status) {
+                    sweetError(label.list+message.ajaxLoadError);
+                }
+            },
+            columns: [
+                {title: "상품", 		data: "gift_name",   		width: "33%" }
+                ,{title: "발송건 수", data: "total_gift_qty",   	width: "33%" }
+                ,{title: "금액",	    data: "total_gift_ucd",  	width: "33%",
+                    render: function (data) {
+                        return numberWithCommas(data);
+                    }
+                }
+            ],
+            serverSide: true,
+            paging: false,
+            select: false,
+            destroy: false,
+            initComplete: function () {
+            },
+            fnRowCallback: function( nRow, aData ) {
+            },
+            drawCallback: function (settings) {
+            }
+        });
+    }
+
+    function tableParams()
+    {
+        let param = {
+            "year" : selYear.val()
+            ,"month": selMonth.val()
+        }
+
+        return JSON.stringify(param);
+    }
+

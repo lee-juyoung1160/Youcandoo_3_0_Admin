b9031de2e305d57d6d6599cee99e761c3c93a5e0
jeen <iamjeen@yanadoocorp.com>
2021-02-17 16:04:23 +0900

    사이드메뉴 active 관련 스크립트

--- assets/v2/js/common.js
+++ assets/v2/js/common.js
@@ -1,37 +1,61 @@
-$(function (){
 
-    /* 레프트메뉴 열고 닫기 */
-    $('.side-toggle-btn').on('click', function(){
-        $(this).toggleClass('btn-toggle');
-        $('aside').toggleClass('aside-open');
-        $('section').toggleClass('wide-content');
+    $(function (){
+
+        /* 레프트메뉴 열고 닫기 */
+        $('.side-toggle-btn').on('click', function(){
+            $(this).toggleClass('btn-toggle');
+            $('aside').toggleClass('aside-open');
+            $('section').toggleClass('wide-content');
+        });
+
+        /* 레프트메뉴 */
+        /*$('.main-mnu').on('click', function(){
+            $('.sub-mnu').hide();
+            $('.main-mnu').removeClass('active');
+            $(this).addClass('active');
+            $(this).siblings('.sub-mnu').show();
+        });*/
+
+        $('.sub-mnu li').on('click', function(){
+            $('.sub-mnu li').removeClass('active');
+            $(this).addClass('active');
+        });
+
+        /* 모달 */
+        $('.modal-btn').on('click', function(){
+            $('.modal-content').fadeIn();
+            $('.modal-bg').fadeIn();
+        });
+        $('.modal-close').on('click', function(){
+            $('.modal-content').hide();
+            $('.modal-bg').fadeOut();
+        });
+        $('.modal-bg').on('click', function(){
+            $('.modal-content').hide();
+            $('.modal-bg').fadeOut();
+        });
+
+        activeMenu();
     });
 
-    /* 레프트메뉴 */
-    $('.main-mnu').on('click', function(){
+    function onClickParentMenu(obj)
+    {
+        $(obj).siblings().removeClass('active');
         $('.sub-mnu').hide();
-        $('.main-mnu').removeClass('active');
-        $(this).addClass('active');
-        $(this).siblings('.sub-mnu').show();
-    });
+        $(obj).addClass('active');
+        $(obj).children('.sub-mnu').show();
+    }
 
-    $('.sub-mnu li').on('click', function(){
-        $('.sub-mnu li').removeClass('active');
-        $(this).addClass('active');
-    });
-
-    /* 모달 */
-    $('.modal-btn').on('click', function(){
-        $('.modal-content').fadeIn();
-        $('.modal-bg').fadeIn();
-    });
-    $('.modal-close').on('click', function(){
-        $('.modal-content').hide();
-        $('.modal-bg').fadeOut();
-    });
-    $('.modal-bg').on('click', function(){
-        $('.modal-content').hide();
-        $('.modal-bg').fadeOut();
-    });
-
-});
+    function activeMenu()
+    {
+        let pathName = getPathName();
+        let menuList = $('nav').find('a');
+        $(menuList).each(function () {
+            let menuPath = $(this).attr('href');
+            if (pathName === menuPath)
+            {
+                $(this).parents('li').addClass('active');
+                $(this).parents('.sub-mnu').show();
+            }
+        })
+    }

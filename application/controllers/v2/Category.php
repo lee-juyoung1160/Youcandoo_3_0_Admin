5546b703641861b915ece7da1fd29d5488ba6f38
juyoungLEE <noocgs@yanadoocorp.com>
2021-02-17 18:16:44 +0900

    3.0 카테고리 관리 1차완료

--- application/controllers/v2/Category.php
+++ application/controllers/v2/Category.php
@@ -4,8 +4,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Category extends MY_Controller {
 
     public function index()
-    {
-        $this->load->template('v2/category/category-list');
-    }
+        {
+            $this->load->template('v2/category/category-list');
+        }
+    public function create()
+        {
+            $this->load->template('v2/category/category-create');
+        }
+    public function detail()
+        {
+            $this->load->template('v2/category/category-detail');
+        }
+    public function update()
+        {
+            $this->load->template('v2/category/category-update');
+        }
 }
+
 

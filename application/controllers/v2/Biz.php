28896d4e56c546c7c9ef8393fa44c655b5b453a1
juyoungLee <noogcs@yanadoocorp.com>
2021-02-23 17:12:59 +0900

    비즈 등록/수정/상세/목록 1차완료

---
+++ application/controllers/v2/Biz.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Biz extends MY_Controller {
+
+    public function index()
+    {
+        $this->load->template('v2/biz/biz-list');
+    }
+
+    public function create()
+    {
+        $this->load->template('v2/biz/biz-create');
+    }
+
+    public function detail()
+    {
+        $this->load->template('v2/biz/biz-detail');
+    }
+    public function update()
+    {
+        $this->load->template('v2/biz/biz-update');
+    }
+
+}

c868178c036313942fb66fedd40f4b38a8718595
juyoungLee <noogcs@yanadoocorp.com>
2021-02-23 17:13:25 +0900

    유캔두 픽 두잇 진행중

---
+++ application/controllers/v2/Pic.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pic extends MY_Controller {
+
+    public function index()
+    {
+        $this->load->template('v2/pic-doit/pic-doit-list');
+    }
+
+    public function create()
+    {
+        $this->load->template('v2/pic-doit/pic-doit-create');
+    }
+
+}

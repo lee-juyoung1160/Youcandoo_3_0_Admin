fc9e6ee549f1bac4398d413edb650cbf9897bd4f
juyoungLee <noogcs@yanadoocorp.com>
2021-02-24 15:39:12 +0900

    UCD 진행중

---
+++ application/controllers/v2/Ucd.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Ucd extends MY_Controller {
+
+    public function index()
+        {
+            $this->load->template('v2/ucd/ucd-system-list');
+        }
+
+    public function doit()
+        {
+            $this->load->template('v2/ucd/ucd-doit-list');
+        }
+    public function user()
+        {
+            $this->load->template('v2/ucd/ucd-user-list');
+        }
+    public function waiting()
+        {
+            $this->load->template('v2/ucd/ucd-waiting-list');
+        }
+    public function charge()
+        {
+            $this->load->template('v2/ucd/ucd-charge-list');
+        }
+    public function create()
+        {
+            $this->load->template('v2/ucd/ucd-charge-create');
+        }
+}
+
+

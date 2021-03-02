92d9239d24770943439a6291ae7ad6eb77510fb8
juyoungLee <noogcs@yanadoocorp.com>
2021-03-02 17:37:40 +0900

    오류 관리 진행중

---
+++ application/controllers/v2/Operate.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Operate extends MY_Controller {
+
+    public function index()
+    {
+        $this->load->template('v2/operate/error-list');
+    }
+
+
+}
+
+

9081e215c55fc9f6ab00afa48ea7dd66d8b4efcb
jeen <iamjeen@yanadoocorp.com>
2021-04-28 15:59:06 +0900

    상품상세 api 연동

--- assets/v2/js/gift/gift-detail.js
+++ assets/v2/js/gift/gift-detail.js
@@ -1,10 +1,10 @@
 
 	import { ajaxRequestWithJsonData, isSuccessResp } from '../modules/request.js'
 	import { api } from '../modules/api-url.js';
-	import {btnBack, btnList, btnUpdate, btnDelete } from '../modules/elements.js';
-	import {sweetToast, sweetToastAndCallback, sweetConfirm} from '../modules/alert.js';
+	import {btnBack, btnList, btnUpdate, giftUuid, giftName, contentImage, price, isExposure} from '../modules/elements.js';
+	import {sweetToast} from '../modules/alert.js';
 	import { historyBack, onErrorImage} from "../modules/common.js";
-	import { getPathName, splitReverse, isEmpty } from "../modules/utils.js";
+	import {getPathName, splitReverse, numberWithCommas} from "../modules/utils.js";
 	import { label } from "../modules/label.js";
 	import { message } from "../modules/message.js";
 	import { page } from "../modules/page-url.js";
@@ -14,11 +14,10 @@
 
 	$( () => {
 		/** 상세 불러오기 **/
-		//getDetail();
+		getDetail();
 		/** 이벤트 **/
 		btnBack	 .on('click', function () { historyBack(); });
 		btnList	 .on('click', function () { goListPage(); });
-		btnDelete.on('click', function () { onSubmitDeleteGift(); });
 		btnUpdate.on('click', function () { goUpdatePage(); });
 	});
 
@@ -30,7 +29,7 @@
 			"idx" : giftIdx
 		}
 
-		ajaxRequestWithJsonData(false, url, JSON.stringify(param), getDetailCallback, errMsg, false);
+		ajaxRequestWithJsonData(true, url, JSON.stringify(param), getDetailCallback, errMsg, false);
 	}
 
 	function getDetailCallback(data)
@@ -38,40 +37,17 @@
 		isSuccessResp(data) ? buildDetail(data) : sweetToast(data.msg);
 	}
 
-	let g_gift_uuid;
 	function buildDetail(data)
 	{
-		const { profile_uuid, nickname } = data.data;
+		const { gift_uuid, gift_name, gift_ucd, gift_image_url, is_exposure } = data.data;
 
-		g_gift_uuid = profile_uuid;
+		giftUuid.text(gift_uuid);
+		giftName.text(gift_name);
+		price.text(`${numberWithCommas(gift_ucd)} UCD`);
+		contentImage.attr('src', gift_image_url);
+		isExposure.text(is_exposure);
 
 		onErrorImage();
-	}
-
-	function onSubmitDeleteGift()
-	{
-		sweetConfirm(message.delete, deleteRequest);
-	}
-
-	function deleteRequest()
-	{
-		const url = api.deleteGift;
-		const errMsg = label.delete + message.ajaxError;
-		const param = {
-			"gift_uuid" : g_gift_uuid,
-		}
-
-		ajaxRequestWithJsonData(true, url, JSON.stringify(param), deleteReqCallback, errMsg, false);
-	}
-
-	function deleteReqCallback(data)
-	{
-		sweetToastAndCallback(data, deleteSuccess)
-	}
-
-	function deleteSuccess()
-	{
-		goListPage();
 	}
 
 	function goListPage()

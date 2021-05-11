3a159988f0c7961d697557ee50d2211490d4f2a7
jeen <iamjeen@yanadoocorp.com>
2021-05-11 17:45:26 +0900

    신고 관련 라우터 추가

--- application/controllers/v2/Report.php
+++ application/controllers/v2/Report.php
@@ -9,16 +9,19 @@ class Report extends MY_Controller {
     }
 
     public function talk($Route = "")
-        {
-            switch ($Route) {
-                case "":
-                     $this->load->template('v2/report/talk-list');
-                    break;
-                case "detail" :
-                     $this->load->template('v2/report/talk-detail');
-                    break;
-            }
+    {
+        switch ($Route) {
+            case "":
+                $this->load->template('v2/report/talk-list');
+                break;
+            case "detail_talk" :
+                $this->load->template('v2/report/talk-detail');
+                break;
+            case "detail_action" :
+                $this->load->template('v2/report/talk-detail-action');
+                break;
         }
+    }
 
 }
 

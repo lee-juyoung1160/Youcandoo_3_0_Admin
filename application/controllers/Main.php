1881bf8c795670bdc634617f146418a4bbd8bd6d
jeen <iamjeen@yanadoocorp.com>
2021-08-09 15:02:26 +0900

    v2 리다이렉트

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -5,22 +5,38 @@ class Main extends MY_Controller {
 
 	public function index()
 	{
-        $this->load->layout('v1/main-dashboard-v2');
+//        $this->load->layout('v2/main-dashboard-v2');
+//        $this->load->template('/v2/main-dashboard');
+        $this->load->helper("url");
+        redirect("/v2");
 	}
 
+//    public function login()
+//    {
+//        $this->load->view('v1//login/login');
+//    }
+//
+//    public function join()
+//    {
+//        $this->load->view('v1//login/join');
+//    }
+//
+//    public function auth()
+//    {
+//        $this->load->view('v1//login/mfa');
+//    }
     public function login()
     {
-        $this->load->view('v1//login/login');
+        $this->load->view('/v2/login/login');
     }
 
     public function join()
     {
-        $this->load->view('v1//login/join');
+        $this->load->view('/v2/login/join');
     }
 
     public function auth()
     {
-        $this->load->view('v1//login/mfa');
+        $this->load->view('/v2/login/mfa');
     }
-
 }

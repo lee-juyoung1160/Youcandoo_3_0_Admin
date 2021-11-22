64080935e6726464be4abd08a13107a64b782920
fisher <raebok@yanadoocorp.com>
2021-11-22 11:57:53 +0900

    AWS codeDeploy 파일 timestamp 배포 시간으로 변경

--- aws-scripts/afterInstall.sh
+++ aws-scripts/afterInstall.sh
@@ -3,6 +3,9 @@ rsync --delete-before --verbose --archive /var/www/release/ /htdocs/ > /var/log/
 
 cp /var/www/release/ /htdocs/
 
+# timestamp
+find /htdocs/ -print0 | xargs -0 touch
+
 if [ -d /var/www/release ]; then
     rm -rf /var/www/release
 fi

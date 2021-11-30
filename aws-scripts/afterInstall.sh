3588598003f1df1493b5cfbac620bbd1e3f1a924
fisher <raebok@yanadoocorp.com>
2021-11-30 14:28:12 +0900

    AWS CodeDeploy 배포 시 cp 명령어 rsync로 변경

--- aws-scripts/afterInstall.sh
+++ aws-scripts/afterInstall.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 rsync --delete-before --verbose --archive /var/www/release/ /htdocs/ > /var/log/deploy.log
 
-cp -R /var/www/release/ /htdocs/
+sync -avz /var/www/release/ /htdocs/
 chown yanadoo:yanadoo /htdocs/
 
 # timestamp
-find /htdocs/ -print0 | xargs -0 touch
+#find /htdocs/ -print0 | xargs -0 touch
 
 if [ -d /var/www/release ]; then
     rm -rf /var/www/release
